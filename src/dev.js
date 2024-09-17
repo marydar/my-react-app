da8c3e49f288630542776f61e9ca1911d308247b
Maryam Dar <maryamdar1384@gmail.com>
2024-09-17 15:30:30 +0330

    projects class and array

--- src/dev.js
+++ src/dev.js
@@ -3,8 +3,19 @@
 import { useRef } from 'react';
 import './App.css';
 import { motion, useScroll, useTransform} from "framer-motion";
+import Project from './project';
 
-function Square(){
+function makeProject(){
+    const project = new Project("music player", "jan 2024", "this is music player clone", "#java #javafx","links", "imgurl");
+    return project;
+}
+const projects = [];
+function addProject(title, date, desc, hashtags, links, imgUrl){
+    console.log(projects.length);
+    projects.push(new Project(title, date, desc, hashtags, links, imgUrl));
+}
+
+function Square(props){
     const ref = useRef(null);
     const { scrollYProgress } = useScroll({
         target: ref,
@@ -12,6 +23,7 @@ function Square(){
     });
     const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.7, 1]);
     const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    // const project = makeProject();
     return(
         <motion.div className='square'
             ref = {ref}
@@ -20,11 +32,22 @@ function Square(){
                 opacity: opacityProgress,
             }}
         >
+            
+                <p className='p'>{props.title}</p>
+                <p className='p'>{props.date}</p>
+                <p className='p'>{props.desc}</p>
+                <p className='p'>{props.hashtags}</p>
+                {/* <a class="ee" href="https://github.com/marydar "  target="_blank"><i class="fa fa-github" style="font-size:20px;color:rgb(255, 255, 255)"></i></a> */}
         </motion.div>
     );
 };
 
 function Dev()  {
+    addProject("music player", "jan 2024", "this is music player clone", "#java #javafx","links", "imgurl");
+    addProject("music player", "jan 2024", "this is music player clone", "#java #javafx","links", "imgurl");
+    addProject("music player", "jan 2024", "this is music player clone", "#java #javafx","links", "imgurl");
+    addProject("music player", "jan 2024", "this is music player clone", "#java #javafx","links", "imgurl");
+    // const project = makeProject();
     return (
         <>
         <div className='navHolder'>
@@ -36,15 +59,13 @@ function Dev()  {
             <div className='dev'>computer engineering student</div>
         </div>
         <div className='center'>
-            <h1 className='projTitle'>PROJRCTS</h1>
+            <div className='projTitle'>PROJRCTS</div>
             <div className='projects'>
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
+                {projects.map((project) =>  <Square title={project.title} desc={project.desc} date={project.date} hashtags={project.hashtags}/> )}
             </div>
+        </div>
+        <div>
+            <h1 style={{marginTop: "1100px", marginBottom: "500px"}}>welcome haha</h1>
         </div>
         </>
     );
---
+++ src/project.js
@@ -0,0 +1,10 @@
+export default class Project {
+    constructor(title, date, desc, hashtags, links, imgUrl) {
+      this.title = title;
+      this.date = date;
+      this.desc = desc;
+      this.hashtags = hashtags;
+      this.links = links;
+      this.imgUrl = imgUrl;
+    }
+}
